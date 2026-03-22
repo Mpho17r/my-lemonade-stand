bd936e85a9383afa11a7faf2b557ae172cbbaac5
Mpho17r <142950205+Mpho17r@users.noreply.github.com>
2026-03-22 23:03:22 +0200

    Fix typo in print statement

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # This is like your lemonade recipe!
-prinnt("🍋 Welcome to my Lemonade Stand! 🍋")
+print("🍋 Welcome to my Lemonade Stand! 🍋")
 print("Would you like some lemonade?")
 answer = input("Type YES or NO: ")
 
