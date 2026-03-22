4e495c67ee2bb07ce5d998a8b9cfb4dc16867230
Mpho17r <142950205+Mpho17r@users.noreply.github.com>
2026-03-22 22:52:45 +0200

    Add lemonade stand greeting and interaction

---
+++ app.py
@@ -0,0 +1,9 @@
+# This is like your lemonade recipe!
+print("🍋 Welcome to my Lemonade Stand! 🍋")
+print("Would you like some lemonade?")
+answer = input("Type YES or NO: ")
+
+if answer == "YES":
+    print("Here's your lemonade! 🥤")
+else:
+    print("Come back if you change your mind! 🌞")
